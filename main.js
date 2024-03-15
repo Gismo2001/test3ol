4ebc0c5171da45502906695bfedeb58ba79c3bf5
Gismo2001 <rankenpunk@googlemail.com>
2024-03-15 23:37:16 +0100

    neu

--- main.js
+++ main.js
@@ -1,43 +1,59 @@
 import 'ol/ol.css'; // Sie müssen die OpenLayers-CSS-Datei weiterhin importieren, da das geo.okapi-Framework OpenLayers verwendet
-new okapi.MapBuilder()
-        .setLayers({
-            baseLayers: [
-                {
-                    type: 'BKG',
-                    name: 'WebAtlasDE',
-                    ref: 'webatlasde_light',
-                    visibility: true,
-                    minResolution: 0.0001,
-                    maxResolution: 156545
-                }
-            ],
-            overlays: [
-                {
-                    type: 'MARKER',
-                    name: 'Marker Leipzig',
-                    visibility: true,
-                    srsName: 'EPSG:4326',
-                    markers: [
-                        {
-                            coordinates: {
-                                lat: 50.091176,
-                                lon: 8.663281
-                            },
-                            content: '<h3>Zentrale Dienststelle in Frankfurt am Main</h3><p>Bundesamt für Kartographie und Geodäsie<br>Richard-Strauss-Allee 11<br>60598 Frankfurt am Main<br>Deutschland</p>'
 
-                        },
-                        {
-                            coordinates: {
-                                lat: 51.354210,
-                                lon: 12.374295
-                            },
-                            content: '<h3>Außenstelle Leipzig</h3><p>Bundesamt für Kartographie und Geodäsie<br> - Außenstelle Leipzig -<br>Karl-Rothe-Straße 10-14<br>04105 Leipzig<br>Deutschland</p>'
-                        }
-                    ],
-                    minResolution: 0.0001,
-                    maxResolution: 156545,
-                    edit: false
-                }
-            ]
-        })
-        .create();
+new okapi.MapBuilder()
+  .setTarget('map')             
+  .setView({
+      projection: 'EPSG:25832'  
+  })
+  .setLayers({
+      baseLayers: [
+          {
+              type: 'BKG',
+              name: 'TopPlusOpen',
+              ref: 'topplus_open',
+              visibility: true
+          }
+      ],
+      overlays: [
+        {
+            type: 'GPS',
+            name: 'GPS Layer',
+            url: 'data/exp_bw_weh.gpx',
+            visibility: true,
+            minResolution: 0.0001,
+            maxResolution: 156545,
+            edit: false
+        }
+    ]
+  })
+  .setView({
+    projection: 'EPSG:25832',
+    center: {
+        lon: 472624.676559,
+        lat: 5554554.520574
+    },
+    zoom: 9
+})
+  .setControls({
+    panelPosition: 'right',
+      tools: {
+        layerSwitcher: {
+          active: true,
+          style: 'customLayerSwitcher',
+          editStyle: false,
+          changeVisibility: true,
+          showWMSLayers: true,
+          changeOrder: true,
+          openLevel: 0
+           },
+          copyright: {active: true},
+          //copyCoordinates: { active: true },
+          showAttributes: {active: true},
+          //showCoordinates: {active: true},
+          zoom: {active: true}
+          
+      }
+  })
+  .create();
+
+ 
