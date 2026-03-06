508bfcea17c9e17b1cd72a26f1200da230da913b
Gismo2001 <rankenpunk@googlemail.com>
2026-03-06 18:02:16 +0100

    test

---
+++ dist/assets/zstd-BxL6sIcE.js
@@ -0,0 +1,2 @@
+import{Z as r}from"./zstddec-stream.modern--lB-NqY4.js";import{B as o}from"./basedecoder-B2c5_Eok.js";const d=new r;class a extends o{decodeBlock(e){return d.decode(new Uint8Array(e)).buffer}}export{a as default,d as zstd};
+//# sourceMappingURL=zstd-BxL6sIcE.js.map
---
+++ dist/assets/zstd-DIHBpvq_.js
@@ -0,0 +1,2 @@
+import{Z as r}from"./zstddec-stream.modern--lB-NqY4.js";import{B as o}from"./basedecoder-Dx75wktK.js";const d=new r;class a extends o{decodeBlock(e){return d.decode(new Uint8Array(e)).buffer}}export{a as default,d as zstd};
+//# sourceMappingURL=zstd-DIHBpvq_.js.map
